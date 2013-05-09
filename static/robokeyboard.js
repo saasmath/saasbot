27cccf6ac6dfdd3305fa8e799c703c76a326bba5
Gary Anderson <git@garyvictoranderson.com>
2013-05-09 18:23:55 +0000

    update robokeyboard.js for more movements

--- static/robokeyboard.js
+++ static/robokeyboard.js
@@ -4,11 +4,14 @@ $(document).ready(function() {
         console.log(event.which);
         if (event.which === 119)
         {
-            $('#result').load('/forward');
+        //    $('#result').load('/forward');
+        //    $('#result').load('/webbytes?bytes=137,0,8,128,0');
+            $('#result').load('/webbytes?bytes=137,1,144,128,0');
         }
         else if (event.which === 115)
         {
-            $('#result').load('/reverse');
+        //    $('#result').load('/reverse');
+        $('#result').load('/webbytes?bytes=137,255,0,128,0');
         }
         else if (event.which === 97)
         {
@@ -17,6 +20,10 @@ $(document).ready(function() {
         else if (event.which === 100)
         {
             $('#result').load('/right');
+        }
+        else if (event.which === 32)
+        {
+            $('#result').load('/webbytes?bytes=137,0,0,128,0');
         }
     });
 });
