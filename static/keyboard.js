ef9f88a67254873b551ca62fe1e03bf92db62216
Gary Anderson <git@garyvictoranderson.com>
2013-06-01 12:27:52 +0000

    keyboard.js prevent default and add brake

--- static/keyboard.js
+++ static/keyboard.js
@@ -2,44 +2,58 @@ $(document).ready(function() {
 
     $('body').keydown(function(event) {
         console.log(event.which);
-
-        if (event.which === 87)
+        if (event.which === 87) //w
         {
             $('#result').load('/webbytes?bytes=145,0,50,0,50');
+            event.preventDefault();
         }
-        else if (event.which === 83)
+        else if (event.which === 83) //s
         {
-            //    $('#result').load('/reverse');
             $('#result').load('/webbytes?bytes=145,255,206,255,206');
+            event.preventDefault();
+
         }
-        else if (event.which === 65)
+        else if (event.which === 65) //a
         {
             $('#result').load('/webbytes?bytes=145,0,50,255,206');
+            event.preventDefault();
+
         }
-        else if (event.which === 68)
+        else if (event.which === 68) //d
         {
             $('#result').load('/webbytes?bytes=145,255,206,0,50');
+            event.preventDefault();
+
         }
-        else if (event.which === 32)
+        else if (event.which === 32) //space bar
         {
             $('#result').load('/webbytes?bytes=145,0,0,0,0');
+            event.preventDefault();
         }
-
-        else if (event.which === 38)
+        else if (event.which === 66) //b
+        {
+            $('#result').load('/brake');
+            event.preventDefault();
+        }
+        else if (event.which === 38) //up arrow
         {
             $('#result').load('/accelerate');
+            event.preventDefault();
         }
-        else if (event.which === 40)
+        else if (event.which === 40) //down arrow
         {
             $('#result').load('/decelerate');
+            event.preventDefault();
         }
-        else if (event.which === 37)
+        else if (event.which === 37) //left arrow
         {
             $('#result').load('/goleft');
+            event.preventDefault();
         }
-        else if (event.which === 39)
+        else if (event.which === 39) //right arrow
         {
             $('#result').load('/goright');
+            event.preventDefault();
         }
     });
 
